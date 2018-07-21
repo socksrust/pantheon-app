c988136fe0ec33d2094d5a5c1a5c1fefad16f959
STENIO FREITAS <stenio.wagner1@gmail.com>
2018-07-21 13:38:52 -0300

    fix:(address-input) - Get Geocode from an invalid Address (Address + Number)

--- src/components/Input.js
+++ src/components/Input.js
@@ -8,7 +8,7 @@ const InputWrapper = styled.View`
   padding: 10px;
   border-bottom-color: ${props => props.theme.colors.secondaryColor};
   border-bottom-width: 2.5;
-  margin-top: 30px;
+  margin-top: 24px;
 `;
 
 const RegisterTextInput = styled(TextInputMask).attrs({
@@ -20,7 +20,6 @@ const RegisterTextInput = styled(TextInputMask).attrs({
   height: 40;
   width: 100%;
   font-size: 20;
-  padding-top:2;
   color: white;
 `;
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,23 @@
-export const getLocation = (
-  address: string,
-  number: string,
-  cep: string,
-  setLocation: (address: string, lat: number, lng: number) => void,
-) => {
-  const newAddress = `${address},${number}-${cep}`;
-  const url = `https://maps.google.com/maps/api/geocode/json?address=${newAddress}&sensor=false`;
-  fetch(url, {})
-    .then(function(res) {
-      return res.text();
-    })
-    .then(function(body) {
-      const geoLocation = JSON.parse(body);
-      console.log(geoLocation);
-      const formatted_address = geoLocation.results[0].formatted_address;
-      const lat = geoLocation.results[0].geometry.location.lat;
-      const lng = geoLocation.results[0].geometry.location.lng;
+export const getLocation = async (zip: string, number: string) => {
+  const url = `https://maps.google.com/maps/api/geocode/json?address=${zip}&sensor=false`;
 
-      setLocation(formatted_address, lat, lng);
-    });
+  const res = await fetch(url);
+  const location = await res.json();
+
+  const { geometry, formatted_address } = location.results[0];
+  const { lat, lng } = geometry.location;
+
+  const indexStartAddress = formatted_address.indexOf('-');
+  const indexEndNeighborhood = formatted_address.indexOf(',');
+
+  const address =
+    formatted_address.substring(0, indexStartAddress - 1) +
+    `, ${number} ` +
+    formatted_address.substring(indexStartAddress, indexEndNeighborhood);
+
+  return {
+    address,
+    lat,
+    lng,
+  };
 };
